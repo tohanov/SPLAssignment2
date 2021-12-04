df1dfa55043c39f686e3baf14fb980c457e120c1
idanhalp <idanhal27@gmail.com>
2021-12-04 15:44:35 +0200

    MessageBusImpl:added basic implementation of class

--- src/main/java/bgu/spl/mics/MessageBusImpl.java
+++ src/main/java/bgu/spl/mics/MessageBusImpl.java
@@ -1,11 +1,53 @@
 package bgu.spl.mics;
 
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Queue;
+
+import javax.swing.text.AbstractDocument.BranchElement;
+
+import bgu.spl.mics.application.messages.PublishConferenceBroadcast;
+import bgu.spl.mics.application.messages.PublishResultsEvent;
+import bgu.spl.mics.application.messages.TickBroadcast;
+
+
+
 /**
  * The {@link MessageBusImpl class is the implementation of the MessageBus interface.
  * Write your implementation here!
  * Only private fields and methods can be added to this class.
  */
 public class MessageBusImpl implements MessageBus {
+
+	private static MessageBusImpl instance= null;
+	
+	HashMap<MicroService,Queue<Message>> microServicesHashMap;
+	
+	HashMap<Class<? extends Broadcast>,LinkedList<MicroService>> broadcastHashMap; 
+
+	private MessageBusImpl(){
+	
+		microServicesHashMap=new HashMap<>();
+		
+		broadcastHashMap=new HashMap<>();
+		broadcastHashMap.put(new PublishConferenceBroadcast().getClass(), new LinkedList<>());
+		broadcastHashMap.put(new TickBroadcast().getClass(), new LinkedList<>());
+		
+	
+
+		
+		
+
+	}
+
+	public static MessageBusImpl getInstance(){
+
+		if(instance==null)
+			instance=new MessageBusImpl();
+
+		return instance;
+
+	}
 
 	@Override
 	public <T> void subscribeEvent(Class<? extends Event<T>> type, MicroService m) {
@@ -15,8 +57,9 @@ public class MessageBusImpl implements MessageBus {
 
 	@Override
 	public void subscribeBroadcast(Class<? extends Broadcast> type, MicroService m) {
-		// TODO Auto-generated method stub
-
+		
+		broadcastHashMap.get(type).addLast(m);
+		
 	}
 
 	@Override
@@ -27,8 +70,11 @@ public class MessageBusImpl implements MessageBus {
 
 	@Override
 	public void sendBroadcast(Broadcast b) {
-		// TODO Auto-generated method stub
 
+		LinkedList<MicroService> ls=broadcastHashMap.get(b.getClass());
+
+		for(MicroService m: ls)
+			microServicesHashMap.get(m).add(b);
 	}
 
 	
@@ -40,13 +86,16 @@ public class MessageBusImpl implements MessageBus {
 
 	@Override
 	public void register(MicroService m) {
-		// TODO Auto-generated method stub
+
+		Queue<Message> ls=new LinkedList<Message>();
+		microServicesHashMap.put(m, ls);
 
 	}
 
 	@Override
 	public void unregister(MicroService m) {
-		// TODO Auto-generated method stub
+		
+		microServicesHashMap.remove(m);
 
 	}
 
