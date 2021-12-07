2fae95a73f322d3cda27a6ef078c82e59d7a5204
tohanov <nope>
2021-12-07 23:55:55 +0200

    added gson as a dependancy

--- src/main/java/bgu/spl/mics/application/CRMSRunner.java
+++ src/main/java/bgu/spl/mics/application/CRMSRunner.java
@@ -5,6 +5,8 @@ import bgu.spl.mics.MicroService;
 import bgu.spl.mics.application.messages.PublishResultsEvent;
 import bgu.spl.mics.example.messages.ExampleEvent;
 import bgu.spl.mics.example.services.ExampleBroadcastListenerService;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 /** This is the Main class of Compute Resources Management System application. You should parse the input file,
  * create the different instances of the objects, and run the system.
